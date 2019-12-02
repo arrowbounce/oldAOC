3ab8afe18cf50c001cbe0632864b8db3ec5ed61b
arrowbounce <ts63280@gmail.com>
2019-12-02 14:22:28 -0800

    Day 1 part 2

--- 2015/Day1.py
+++ 2015/Day1.py
@@ -1,7 +1,17 @@
 total = 0
+pos = 1
 with open("day1") as f:
 	for line in f:
-		up = line.count('(')
-		down = line.count(')')
-		total = up - down
+		for ch in line:
+			if ch =='(':
+				total += 1
+			else:
+				total -= 1
+			if total == -1:
+				print pos
+				break
+			pos += 1
+		# up = line.count('(')
+		# down = line.count(')')
+		# total = up - down
 print total
