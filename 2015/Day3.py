c4f1388110974582a34de4c1816dd2c08b04b4d2
arrowbounce <ts63280@gmail.com>
2019-12-02 14:53:11 -0800

    Day 3 part 2

--- 2015/Day3.py
+++ 2015/Day3.py
@@ -1,17 +1,32 @@
 x=0
 y=0
+x2=0
+y2=0
+mode = 0
 visited = {(0,0)}
 with open("day3") as f:
 	for line in f:
 		for ch in line:
-			if ch =='^':
-				y += 1
-			elif ch =='v':
-				y -= 1
-			elif ch == '>':
-				x += 1
+			if mode == 0:
+				if ch =='^':
+					y += 1
+				elif ch =='v':
+					y -= 1
+				elif ch == '>':
+					x += 1
+				else:
+					x -= 1
+				visited.add((x,y))
 			else:
-				x -= 1
-			visited.add((x,y))
+				if ch =='^':
+					y2 += 1
+				elif ch =='v':
+					y2 -= 1
+				elif ch == '>':
+					x2 += 1
+				else:
+					x2 -= 1
+				visited.add((x2,y2))
+			mode = 1 - mode
 print visited
 print len(visited)
