0dbfedc7dd12f79558470d9782df6d5007238c81
arrowbounce <ts63280@gmail.com>
2019-12-03 15:12:17 -0800

    Day 8 pt 2

--- 2015/Day8.py
+++ 2015/Day8.py
@@ -2,15 +2,16 @@ diff = 0
 with open("Day8") as f:
 	for line in f:
 		i = 0
+		diff += 2
 		while i< len(line):
-			if line[i] == "\"":
+			if line[i] == "\"" or line[i] == "\\":
 				diff += 1
-			elif line[i] == "\\":
-				i += 1
-				if line[i] == "x":
-					diff += 3
-					i += 2
-				else:
-					diff += 1
+			# elif line[i] == "\\":
+			# 	i += 1
+			# 	if line[i] == "x":
+			# 		diff += 3
+			# 		i += 2
+			# 	else:
+			# 		diff += 1
 			i += 1
 print diff
