d473bf75934f8527c02c481e9ae8ea792b5d05bb
arrowbounce <ts63280@gmail.com>
2019-12-02 20:17:10 -0800

    Day 6 pt 2

--- 2015/Day6.py
+++ 2015/Day6.py
@@ -8,17 +8,18 @@ with open("Day6") as f:
 			xy2 = h[3].split(",")
 			x = slice(int(xy1[0]), int(xy2[0])+1)
 			y = slice(int(xy1[1]), int(xy2[1])+1)
-			array[x,y] ^= 1
+			array[x,y] += 2
 		elif h[1] == "on":
 			xy1 = h[2].split(",")
 			xy2 = h[4].split(",")
 			x = slice(int(xy1[0]), int(xy2[0])+1)
 			y = slice(int(xy1[1]), int(xy2[1])+1)
-			array[x,y] = 1
+			array[x,y] += 1
 		else:
 			xy1 = h[2].split(",")
 			xy2 = h[4].split(",")
 			x = slice(int(xy1[0]), int(xy2[0])+1)
 			y = slice(int(xy1[1]), int(xy2[1])+1)
-			array[x,y] = 0
+			array[x,y] -= 1
+		array[array<0] = 0
 print sum(sum(array))
