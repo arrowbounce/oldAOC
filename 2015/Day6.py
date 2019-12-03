7e03428413dec1cdf6d7d902f8e2ccdf443c795b
arrowbounce <ts63280@gmail.com>
2019-12-02 20:13:46 -0800

    Day 6 pt 1

---
+++ 2015/Day6.py
@@ -0,0 +1,24 @@
+import numpy
+array = numpy.zeros((1000,1000), dtype = numpy.int)
+with open("Day6") as f:
+	for line in f:
+		h = line.split()
+		if h[0] == "toggle":
+			xy1 = h[1].split(",")
+			xy2 = h[3].split(",")
+			x = slice(int(xy1[0]), int(xy2[0])+1)
+			y = slice(int(xy1[1]), int(xy2[1])+1)
+			array[x,y] ^= 1
+		elif h[1] == "on":
+			xy1 = h[2].split(",")
+			xy2 = h[4].split(",")
+			x = slice(int(xy1[0]), int(xy2[0])+1)
+			y = slice(int(xy1[1]), int(xy2[1])+1)
+			array[x,y] = 1
+		else:
+			xy1 = h[2].split(",")
+			xy2 = h[4].split(",")
+			x = slice(int(xy1[0]), int(xy2[0])+1)
+			y = slice(int(xy1[1]), int(xy2[1])+1)
+			array[x,y] = 0
+print sum(sum(array))
